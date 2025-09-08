2e89d54015157a6f8af103f326514a0bb1b3d4ba
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-09-08 23:35:08 +0530

    deployement setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: mode=="production"?"/fiji-table-book/":"",
+   base: mode=="production"?"/fiji-sun-table-book/":"",
   server: {
     host: "::",
     port: 8080,
